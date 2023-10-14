39bf6116d30ff2a44d6876700a330b1cf86e4af5
Pedro Henrique Pereira de Melo <47577857+indiebr@users.noreply.github.com>
2023-10-14 21:04:47 +0100

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+group = "org.example"
+"1.0-SNAPSHOT".also { version = it }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    // https://mvnrepository.com/artifact/com.googlecode.lanterna/lanterna
+    implementation (group= "com.googlecode.lanterna", name= "lanterna", version= "3.2.0-alpha1")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("Application")
+}
+
